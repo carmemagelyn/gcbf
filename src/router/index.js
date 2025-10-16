68d842e7ae9d2950be840cd9fdc58a589a481052
carmemagelyn <carmemagelyn@gmail.com>
2025-10-16 22:57:53 +0800

    link update

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import { isAuthenticated, getCurrentUser, isAdmin } from '../utils/auth'
 
 // Import views
@@ -52,7 +52,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes
 })
 
