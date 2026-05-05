319f4234b7a97048554c2ddfeb6e814715460a35
carmemagelyn <carmemagelyn@gmail.com>
2026-05-05 15:30:16 +0800

    update api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -158,10 +158,9 @@
         <div class="map-container shadow-lg position-relative">
           <div class="map-overlay"></div>
           <iframe 
-            src="https://www.google.com/maps/embed/v1/place?key=YOUR_API_KEY&q=Graced+Community+Bible+Fellowship,Iloilo,Philippines&zoom=16" 
-            style="border:0; width: 100%; height: 450px;" 
-            allowfullscreen="" 
-            loading="lazy" 
+            src="https://www.google.com/maps?q=Graced+Community+Bible+Fellowship,Iloilo,Philippines&output=embed"
+            style="border:0; width: 100%; height: 450px;"
+            loading="lazy"
             referrerpolicy="no-referrer-when-downgrade">
           </iframe>
         </div>
